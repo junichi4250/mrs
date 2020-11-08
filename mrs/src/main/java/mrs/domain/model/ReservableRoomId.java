a300b448f19ad583f95dfc66550f35c74b727887
junkatou <jkky0225@gmail.com>
2020-11-08 14:59:36 +0900

    beanの追加

--- mrs/src/main/java/mrs/domain/model/ReservableRoomId.java
+++ mrs/src/main/java/mrs/domain/model/ReservableRoomId.java
@@ -1,5 +1,8 @@
 package mrs.domain.model;
 
+import java.io.Serializable;
+import java.time.LocalDate;
+
 @Embeddable
 public class ReservableRoomId implements Serializable{
 
@@ -13,7 +16,46 @@ public class ReservableRoomId implements Serializable{
 	}
 
 	public ReservableRoomId() {
-		Í
+
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((reservedDate == null) ? 0 : reservedDate.hashCode());
+		result = prime * result + ((roomId == null) ? 0 : roomId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		ReservableRoomId other = (ReservableRoomId) obj;
+		if (reservedDate == null) {
+			if (other.reservedDate != null) {
+				return false;
+			}
+		} else if (!reservedDate.equals(other.reservedDate)) {
+			return false;
+		}
+
+		if (roomId == null) {
+			if (other.roomId != null) {
+				return false;
+			} else if (roomId.equals(other.roomId)) {
+				return true;
+			}
+			return true;
+		}
 	}
 
 
